3fd282c0a90068763b23e1c82a04d22732c179e6
FTT\mikm <etherealtsunami@gmail.com>
2017-01-16 18:54:41 +0900

    Waiting Room basic setup

--- MatchingServer-CSharp/Classes/WaitingRoom.cs
+++ MatchingServer-CSharp/Classes/WaitingRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,52 @@ namespace MatchingServer_CSharp.Classes
 {
     class WaitingRoom
     {
+        struct PlayerIdentification
+        {
+            string matchingServerID;
+            string playerID;
+        }
+
+        enum MatchState
+        {
+            UnSet = 0,
+            UnMatched = 1,
+            MatchedButUnconfirmed,
+            ConfirmedWithServer
+        }
+
+        struct PlayerInfo
+        {
+            PlayerIdentification identification;
+            MatchState state;
+            float metric;
+        }
+
+        struct Match
+        {
+            PlayerIdentification playerOne;
+            PlayerIdentification playerTwo;
+        }
+
+        struct MatchingServer
+        {
+            OrderedDictionary playerQueue;      // <string, PlayerInfo>
+            float latency;
+
+            public MatchingServer (float latencyValue)
+            {
+                playerQueue = new OrderedDictionary();
+                latency = latencyValue;
+            }
+        }
+
+
         //###########################################
         //             Fields/Properties
         //###########################################
         private Logs logs;
         private ConnectionManager connectionManager;
+        private Dictionary<string, MatchingServer> serverList = new Dictionary<string, MatchingServer>();
 
         //Properties
         public bool IsInitialized { get; private set; } = false;
